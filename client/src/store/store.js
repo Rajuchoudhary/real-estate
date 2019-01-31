1726cf40a6d02538938e62152ec0f5586501daf0
Raju <awesomedesign007@gmail.com>
2019-01-31 13:27:50 +0530

    fixed one error

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -6,8 +6,7 @@ const store = createStore(
   rootReducer,
   {},
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
   )
 );
 
